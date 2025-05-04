a42de0c360373adbfad07c65334e2d5a26f778c5
Ethan-Good <goethang@gmail.com>
2025-05-04 01:07:16 -0600

    implemented reset board function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -55,6 +55,54 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        //clear board
+        for (int row = 0; row < 8; row++){
+            for (int col = 0; col < 8; col++){
+                squares[row][col] = null;
+            }
+        }
+
+        ChessGame.TeamColor white = ChessGame.TeamColor.WHITE;
+        ChessPiece.PieceType rook = ChessPiece.PieceType.ROOK;
+        ChessPiece.PieceType knight = ChessPiece.PieceType.KNIGHT;
+        ChessPiece.PieceType bishop = ChessPiece.PieceType.BISHOP;
+        ChessPiece.PieceType king = ChessPiece.PieceType.KING;
+        ChessPiece.PieceType queen = ChessPiece.PieceType.QUEEN;
+        ChessPiece.PieceType pawn = ChessPiece.PieceType.PAWN;
+
+        addPiece(new ChessPosition(1,1), new ChessPiece(white, rook));
+        addPiece(new ChessPosition(1,2), new ChessPiece(white, knight));
+        addPiece(new ChessPosition(1,3), new ChessPiece(white, bishop));
+        addPiece(new ChessPosition(1,4), new ChessPiece(white, queen));
+        addPiece(new ChessPosition(1,5), new ChessPiece(white, king));
+        addPiece(new ChessPosition(1,6), new ChessPiece(white, bishop));
+        addPiece(new ChessPosition(1,7), new ChessPiece(white, knight));
+        addPiece(new ChessPosition(1,8), new ChessPiece(white, rook));
+        addPiece(new ChessPosition(2,1), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,2), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,3), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,4), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,5), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,6), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,7), new ChessPiece(white, pawn));
+        addPiece(new ChessPosition(2,8), new ChessPiece(white, pawn));
+
+        ChessGame.TeamColor black = ChessGame.TeamColor.BLACK;
+        addPiece(new ChessPosition(8,1), new ChessPiece(black, rook));
+        addPiece(new ChessPosition(8,2), new ChessPiece(black, knight));
+        addPiece(new ChessPosition(8,3), new ChessPiece(black, bishop));
+        addPiece(new ChessPosition(8,4), new ChessPiece(black, queen));
+        addPiece(new ChessPosition(8,5), new ChessPiece(black, king));
+        addPiece(new ChessPosition(8,6), new ChessPiece(black, bishop));
+        addPiece(new ChessPosition(8,7), new ChessPiece(black, knight));
+        addPiece(new ChessPosition(8,8), new ChessPiece(black, rook));
+        addPiece(new ChessPosition(7,1), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,2), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,3), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,4), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,5), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,6), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,7), new ChessPiece(black, pawn));
+        addPiece(new ChessPosition(7,8), new ChessPiece(black, pawn));
     }
 }
